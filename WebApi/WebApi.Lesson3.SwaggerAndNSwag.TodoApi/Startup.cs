d54d67c23267cdb4558cf4ce8422c4db450b4c4f
Kim Phuong Tran <kimphuong.tran@acomsolutions.com>
2019-07-24 14:23:06 +0700

    Add Swagger

--- WebApi/WebApi.Lesson3.SwaggerAndNSwag.TodoApi/Startup.cs
+++ WebApi/WebApi.Lesson3.SwaggerAndNSwag.TodoApi/Startup.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -11,6 +13,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
 using WebApi.Lesson3.SwaggerAndNSwag.TodoApi.Models;
 
 namespace WebApi.Lesson3.SwaggerAndNSwag.TodoApi
@@ -32,7 +35,30 @@ namespace WebApi.Lesson3.SwaggerAndNSwag.TodoApi
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "TODO API", Version = "v1",
+                        Description = "A simple example ASP.NET Core Web API",
+                        TermsOfService = new Uri("http://example.com/terms"),
+                        Contact = new OpenApiContact
+                        {
+                            Name = "Kim Phuong",
+                            Email = "kimphuong.tran@ezitvn.com",
+                            Url = new Uri("https://facebook.com/TranThiKimPhuong"),
+                        },
+                        License = new OpenApiLicense
+                        {
+                            Name = "Use under LICX",
+                            Url = new Uri("https://example.com/License"),
+                        }
+                });
 
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -47,6 +73,15 @@ namespace WebApi.Lesson3.SwaggerAndNSwag.TodoApi
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
 
             app.UseHttpsRedirection();
             app.UseMvc();
